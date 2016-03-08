22770222c105915d33caf8cb726de2bbeebaa88b
Jack Stein <jacklsteiniv@gmail.com>
2016-03-07 17:12:11 -0800

    Basic map intact; build in search or random functions.

---
+++ js/esri.js
@@ -0,0 +1,9 @@
+//ArcGIS module via Esri
+var map;
+require(["esri/map", "dojo/domReady!"], function(Map) {
+  map = new Map("mapDiv", {
+    center: [-56.049, 38.485],
+    zoom: 3,
+    basemap: "streets"
+  });
+});
