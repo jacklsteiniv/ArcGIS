b06d616032daf7a47c3b8e293435503323ee42f7
Jack Stein <jacklsteiniv@gmail.com>
2016-03-10 19:10:19 -0800

    Linking index.html and js script to ArcGIS map created online. Resolve CORS issue.

--- js/esri.js
+++ js/esri.js
@@ -1,9 +1,17 @@
 //ArcGIS module via Esri
 var map;
-require(["esri/map", "dojo/domReady!"], function(Map) {
-  map = new Map("mapDiv", {
-    center: [-56.049, 38.485],
-    zoom: 3,
-    basemap: "streets"
+
+var dojoConfig = { parseOnLoad:true };
+require(["esri/map", "esri/arcgis/utils", "dojo/domReady!"], function(Map, arcgisUtils) {
+
+  //using map ID from crime map + emphasis on Chicago below from ArcGIS.com.
+
+  arcgisUtils.createMap("7fd2ad9ec0254e9a86226086c3893094", "mapDiv").then(function (response) {
+      map = response.map;
   });
+  // map = new Map("mapDiv", {
+  //   center: [-56.049, 38.485],
+  //   zoom: 3,
+  //   basemap: "streets"
+  // });
 });
